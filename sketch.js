e3f2000963f24eeffadeb0a68e3ec15deaf3035d
karthik008-cod <72755692+karthik008-cod@users.noreply.github.com>
2021-07-12 16:43:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var M1, M2, M3, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M15, M16, M17, M18, M19, M20, M21, M22, M23, M24, M25, M26, M27, M28, M28, M29, M30, M31, M32, M33, M33, M34, M35, M36, M37, M38, M100, M101, M102, M103;
+function setup(){
+  canvas = createCanvas (350, 350);
+  M100 = createSprite(100, 5, 500, 10);
+  M101 = createSprite(100, 345, 500, 10);
+  M102 = createSprite(345, 100, 10, 500);
+  M103 = createSprite(5, 250, 10, 500);
+  M1 = createSprite(20, 50, 50, 10);
+  M2 = createSprite(100, 50, 50, 10);
+  M3 = createSprite(180, 50, 50, 10);
+  M4 = createSprite(180, 80, 10, 50);
+  M5 = createSprite(10, 200, 50, 10);
+  M6 = createSprite(55, 225, 10, 10);
+  M7 = createSprite(100, 300, 50, 10);
+  M8 = createSprite(100, 150, 10, 50);
+  M9 = createSprite(150, 200, 50, 10);
+  M10 = createSprite(200, 80, 50, 10);
+  M11 = createSprite(250, 80, 10, 50);
+  M12 = createSprite(320, 100, 50, 10);
+  M13 = createSprite(140, 155, 10, 10);
+  M14 = createSprite(300, 200, 50, 10);
+  M15 = createSprite(300, 170, 10, 50);
+  M16 = createSprite(300, 250, 50, 10);
+  M17 = createSprite(350, 250, 50, 10);
+  M18 = createSprite(180, 180, 10, 50);
+  M19 = createSprite(260, 300, 50, 10);
+  M20 = createSprite(250, 150, 50, 10);
+  M21 = createSprite(225, 150, 10, 50);
+  M22 = createSprite(20, 125, 50, 10);
+  M23 = createSprite(40, 125, 10, 50);
+  M24 = createSprite(100, 125, 50, 10);
+  M25 = createSprite(100, 200, 50, 10);
+  M26 = createSprite(100, 225, 10, 50);
+  M27 = createSprite(77, 255, 55, 10);
+  M28 = createSprite(45, 275, 10, 50);
+  M29 = createSprite(80, 325, 10, 50);
+  M30 = createSprite(180, 320, 10, 50);
+  M31 = createSprite(100, 225, 10, 50);
+  M32 = createSprite(100, 75, 10, 40);
+  M33 = createSprite(100, 225, 10, 50);
+  M34 = createSprite(320, 40, 50, 10);
+  M35 = createSprite(180, 225, 10, 50);
+  M36 = createSprite(165, 250, 40, 10);
+  M37 = createSprite(200, 215, 50, 10);
+  M38 = createSprite(240, 280, 10, 50);
+}
+function draw() {
+  background(200,225,225)
+  drawSprites();
+  
+}
